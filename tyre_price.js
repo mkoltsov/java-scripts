969b5c2c55946c83f40495caa8071779a503acc2
mkoltsov <mikhail.koltsov@appdynamics.com>
2020-05-28 13:29:05 +0200

    changes saved

--- tyre_price.js
+++ tyre_price.js
@@ -4,26 +4,16 @@ const util = require('util');
 const setTimeoutPromise = util.promisify(setTimeout);
 const TOKEN = process.env.TELEGRAM_TOKEN;
 const Tgfancy = require("tgfancy");
-const bot = new Tgfancy(TOKEN, 
-// {
-    // all options to 'tgfancy' MUST be placed under the
-    // 'tgfancy' key, as shown below
-    // tgfancy: {
-    //     option: "value",
-    // },
-// }
-);
-const name = process.env.position || "'Zeetex HP2000 VFM 215/55 R17 98 W XL'" ;
-const js = process.env.selector || `$("div a[title=${name}]").parent().parent().parent().parent().find('.details.right > .shopping > .buy > .price > span.price.size-3').text()`;
-const url = process.env.url || 'https://www.oponeo.pl/wybierz-opony/s=1/letnie/r=1/215-55-r17';
-
-// bot.setWebHook(`${url}/bot${TOKEN}`);
+const bot = new Tgfancy(TOKEN);
+const fs = require('fs');
+const name = process.env.position;
+const js = process.env.selector;
+const url = process.env.url;
 
 function launchChrome(headless=true) {
   return chromeLauncher.launch({
-    // port: 9222, // Uncomment to force a specific port of your choice.
     chromeFlags: [
-      '--window-size=412,732',
+      '--window-size=1920,1080',
       '--disable-gpu',
       '--no-sandbox',
       '--disable-cache',
@@ -34,14 +24,9 @@ function launchChrome(headless=true) {
 
 launchChrome().then(chrome => {
   console.log(`Chrome debuggable on port: ${chrome.port}`);
-  
   setTimeoutPromise(8000, 'foobar').then((value) => {
-    // value === 'foobar' (passing values is optional)
-    // This is executed after about 40 milliseconds.
     chrome.kill();
   });
-
-  
 });
 
 (async function() {
@@ -57,21 +42,23 @@ await Promise.all([Page.enable(), Runtime.enable()]);
 Page.navigate({url: url});
 // Wait for window.onload before doing stuff.
 Page.loadEventFired(async () => {
-  
+
   // Evaluate the JS expression in the page.
   const result = await Runtime.evaluate({expression: js});
+  console.log(result);
+  console.log(url);
+  console.log(js);
+//    const {data} = await Page.captureScreenshot();
+//            fs.writeFileSync('scrot.png', Buffer.from(data, 'base64'));
 
   console.log('Text retrieved: ' + result.result.value);
 
-  // bot.on('message', function onMessage(msg) {
-  //   bot.sendMessage(msg.chat.id, result.result.value);
-  //   console.log(msg.chat.id);
-  // });
+ 
   bot.sendMessage(859059312, `${name} price=${result.result.value}`);
 
   chrome.kill(); // Kill Chrome.
   protocol.close();
-  
+
 });
 
 })();
